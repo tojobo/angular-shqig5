bed219b53aa8306d1ca0e109083943203a03b2c0
tojobo <tojobo22@gmail.com>
2021-12-06 15:29:17 -0500

    1 file modified

--- src/app/my-counter/my-counter.component.ts
+++ src/app/my-counter/my-counter.component.ts
@@ -1,26 +1,27 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-
+import { increment, decrement, reset } from '../counter.actions';
+ 
 @Component({
   selector: 'app-my-counter',
   templateUrl: './my-counter.component.html',
 })
 export class MyCounterComponent {
   count$: Observable<number>;
-
-  constructor() {
-    // TODO: Connect `this.count$` stream to the current store `count` state
+ 
+  constructor(private store: Store<{ count: number }>) {
+    this.count$ = store.select('count');
   }
-
+ 
   increment() {
-    // TODO: Dispatch an increment action
+    this.store.dispatch(increment());
   }
-
+ 
   decrement() {
-    // TODO: Dispatch a decrement action
+    this.store.dispatch(decrement());
   }
-
+ 
   reset() {
-    // TODO: Dispatch a reset action
+    this.store.dispatch(reset());
   }
-}
